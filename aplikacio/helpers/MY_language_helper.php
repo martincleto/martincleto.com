a862a9a045ec8ba5441ce705de984a7077e22966
smartin <smartin@conelpa.es>
2013-04-30 11:18:56 +0200

    Reseting repo

--- aplikacio/helpers/MY_language_helper.php
+++
@@ -1,5 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-// ------------------------------------------------------------------------
-/* End of file language_helper.php */
-/* Location: ./system/helpers/language_helper.php */
